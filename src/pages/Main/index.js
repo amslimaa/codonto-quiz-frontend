73996fded899d00fbdebf6be6c86d430945055ec
Amos Lima <social.amos@gmail.com>
2020-09-21 01:36:23 -0300

    creating initial page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,10 +3,16 @@ import React from 'react';
 import { Container } from './styles';
 
 export default function Main() {
-
   return (
     <Container>
-      <h1>Hello main</h1>
+      <header>
+        <ol>
+          <li>Biosseguranca UFPI</li>
+          <li>Contato </li>
+          <li>Quem somos </li>
+          <li>A missao</li>
+        </ol>
+      </header>
     </Container>
   );
 }
