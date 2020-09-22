d6378888175922723e38ded745760e5b6e7a14f1
Amos Lima <social.amos@gmail.com>
2020-09-22 01:28:45 -0300

    resolve coflicts

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -9,11 +9,7 @@ export default function Main() {
         <ul>
           <li>Biosseguranca UFPI</li>
           <li>Contato</li>
-<<<<<<< HEAD
           <li>Quem somos</li>
-=======
-          <li>Quem somos </li>
->>>>>>> 1c69b7a9b9c8a0a6ebbf8ed4a0c80251cd877a77
           <li>A missao</li>
         </ul>
       </header>
