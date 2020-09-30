9ba9d4b600473482453193f7f9838943f459893c
Amos Lima <social.amos@gmail.com>
2020-09-30 01:58:27 -0300

    edt routes to quiz

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Main from './pages/Main';
+// import Main from './pages/Main';
 import Quiz from './pages/Quiz';
-import Logon from './pages/Logon';
+/* import Logon from './pages/Logon';
 import Profile from './pages/Profile';
 import NewQuestion from './pages/NewQuestion';
-
+ */
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/logon" component={Logon} />
+        <Route exact path="/" component={Quiz} />
+        {/* <Route exact path="/logon" component={Logon} />
         <Route exact path="/profile" component={Profile} />
-        <Route exact path="/new-question" component={NewQuestion} />
+        <Route exact path="/new-question" component={NewQuestion} /> */}
         <Route exact path="/game-quiz" component={Quiz} />
       </Switch>
     </BrowserRouter>
